534481a4adc5d11d0299acc2f7bd1357ef493817
Wayne Myers <wgmyers@gmail.com>
2018-12-19 20:23:49 +0000

    Added sliders to control donut size

--- sketch.js
+++ sketch.js
@@ -1,15 +1,26 @@
 function setup() {
   var myCanvas = createCanvas(1200, 800, WEBGL);
+  /* myCanvas goes in donut div */
   myCanvas.parent('donut');
+
+  /* Light grey wireframe */
   noFill();
   line = color("#DDDDDD");
   stroke(line);
+
+  /* Set up some sliders */
+  radiusSlider = createSlider(5,300,150);
+  radiusSlider.position(20, 20);
+  tubeSlider = createSlider(5,300,150);
+  tubeSlider.position(20, 50);
 }
 
 function draw() {
+  var radius = radiusSlider.value();
+  var tube = tubeSlider.value();
   background(0);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
   rotateZ(frameCount * 0.01);
-  torus(150, 140);
+  torus(radius, tube);
 }
